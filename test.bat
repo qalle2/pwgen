b33416cbf471e0530ffd25a2bda7166d65eb52fb
Kalle <qalle85@gmail.com>
2020-03-17 21:36:07 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,21 +1,36 @@
 @echo off
 cls
 
-echo === test.bat: one password of length 8 ===
-python pwgen.py 8
+echo === test.bat: default settings ===
+python pwgen.py
+python pwgen.py
+python pwgen.py
+python pwgen.py
 echo.
 
-echo === test.bat: two alphanumeric passwords of length 12, no digits 0 or 1 ===
-python pwgen.py --character-sets "uld" --digits "23456789" --count 2 12
+echo === test.bat: length 40; alphanumeric; no 0Oo1Il ===
+python pwgen.py --length 40 --sets uld --uppercase ABCDEFGHJKLMNPQRSTUVWXYZ --lowercase abcdefghijkmnpqrstuvwxyz --digits 23456789
 echo.
 
-echo === test.bat: two Unicode passwords of length 10 ===
-python pwgen.py --character-sets "n" --count 2 10
+echo === test.bat: length 40; all character sets ===
+python pwgen.py --length 40 --sets uldpn
+echo.
+
+echo === test.bat: length 40; uppercase as Unicode ===
+python pwgen.py --length 40 --sets n --unicode 41-5a
 echo.
 
 echo === test.bat: all of these should cause an error ===
-python pwgen.py
-python pwgen.py --nonexistent 8
-python pwgen.py 0
-python pwgen.py --character-sets "" 8
-python pwgen.py --character-sets "u" --uppercase "" 8
+python pwgen.py --length 0
+python pwgen.py --sets ""
+python pwgen.py --sets x
+python pwgen.py --sets u --uppercase ""
+python pwgen.py --sets n --unicode ""
+python pwgen.py --sets n --unicode ","
+python pwgen.py --sets n --unicode 110000
+python pwgen.py --sets n --unicode "0--1"
+python pwgen.py --sets n --unicode 42-41
+echo.
+
+echo === test.bat: help ===
+python pwgen.py --help
