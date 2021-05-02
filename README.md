cf53a0634ed125bf4261328a5e27dfe9d185dfa2
qalle2 <qalle85@gmail.com>
2021-05-02 16:12:53 +0300

    tidy up

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 # pwgen
-Generate a password using the cryptographically strong secrets module.
-
-## Syntax
 ```
-usage: pwgen.py [-h] [-l LENGTH] [-s SETS] [--uppercase UPPERCASE] [--lowercase LOWERCASE] [--digits DIGITS]
-                [--punctuation PUNCTUATION] [--unicode UNICODE]
+usage: pwgen.py [-h] [-l LENGTH] [-s SETS] [--uppercase UPPERCASE] [--lowercase LOWERCASE]
+                [--digits DIGITS] [--punctuation PUNCTUATION] [--unicode UNICODE]
 
 Generate a password using the cryptographically strong secrets module.
 
@@ -12,31 +9,17 @@ optional arguments:
   -h, --help            show this help message and exit
   -l LENGTH, --length LENGTH
                         The length of the password. (default: 10)
-  -s SETS, --sets SETS  Which character sets to use in the password: u=uppercase letters, l=lowercase letters,
-                        d=digits, p=punctuation, n=Unicode codepoints. (default: uld)
+  -s SETS, --sets SETS  Which character sets to use in the password: u=uppercase letters,
+                        l=lowercase letters, d=digits, p=punctuation, n=Unicode. (default: uld)
   --uppercase UPPERCASE
                         The set of uppercase letters. (default: ABCDEFGHIJKLMNOPQRSTUVWXYZ)
   --lowercase LOWERCASE
                         The set of lowercase letters. (default: abcdefghijklmnopqrstuvwxyz)
   --digits DIGITS       The set of digits. (default: 0123456789)
   --punctuation PUNCTUATION
-                        The set of punctuation characters. (default: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~)
-  --unicode UNICODE     The hexadecimal Unicode codepoints (0-10ffff). A hyphen ("-") separates the first and last
-                        codepoint of a range. A comma (",") separates codepoints and ranges. (default: a1-ac,ae-ff)
+                        The set of punctuation characters. (default:
+                        !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~)
+  --unicode UNICODE     A set of characters as ranges of hexadecimal Unicode codepoints
+                        (0-10ffff). A hyphen ('-') separates the first and last codepoint of a
+                        range. A comma (',') separates ranges. (default: a1-ac,ae-ff)
 ```
-
-## Examples
-Generate an alphanumeric ASCII password of length 16:
-```
-C:\>python pwgen.py --length 16 --sets uld
-00WVo2T1Wn1DRFma
-```
-
-Generate a password of length 32 using *Unicode Miscellaneous Symbols and Pictographs* except `PILE OF POO`:
-```
-python pwgen.py --length 32 --sets n --unicode 1f300-1f4a8,1f4aa-1f5ff
-🍤🖄💯🎑💛👃🗈🖠🗂👠🖇🖣🎒🕥🍽🕭🕻🐨🌥🔻📈🔇💕🍳🎧🕄🖛📽🐂💿🎄🏠
-```
-
-## References
-* [Python documentation &ndash; the secrets module &ndash; Recipes and best practices](http://docs.python.org/library/secrets.html#recipes-and-best-practices)
--- pwgen.py
+++ pwgen.py
@@ -1,12 +1,7 @@
-"""Generate a password using the cryptographically strong secrets module."""
-
-import argparse
-import secrets
-import string
-import sys
+import argparse, secrets, string, sys
 
 def parse_arguments():
-    """Parse command line arguments using argparse."""
+    # parse command line arguments using argparse
 
     parser = argparse.ArgumentParser(
         description="Generate a password using the cryptographically strong secrets module.",
@@ -19,7 +14,7 @@ def parse_arguments():
     parser.add_argument(
         "-s", "--sets", default="uld",
         help="Which character sets to use in the password: u=uppercase letters, l=lowercase "
-        "letters, d=digits, p=punctuation, n=Unicode codepoints."
+        "letters, d=digits, p=punctuation, n=Unicode."
     )
     parser.add_argument(
         "--uppercase", default=string.ascii_uppercase, help="The set of uppercase letters."
@@ -35,70 +30,59 @@ def parse_arguments():
     )
     parser.add_argument(
         "--unicode", default="a1-ac,ae-ff",
-        help="The hexadecimal Unicode codepoints (0-10ffff). A hyphen (\"-\") separates the first "
-        "and last codepoint of a range. A comma (\",\") separates codepoints and ranges."
+        help="A set of characters as ranges of hexadecimal Unicode codepoints (0-10ffff). A hyphen "
+        "('-') separates the first and last codepoint of a range. A comma (',') separates ranges."
     )
 
     args = parser.parse_args()
+
     if args.length < 1:
         sys.exit("Invalid password length.")
     if not args.sets:
         sys.exit("Need at least one character set.")
     if set(args.sets) - set("uldpn"):
         sys.exit("No such a character set.")
+
     return args
 
 def parse_codepoint(codepoint):
-    """Parse a hexadecimal Unicode codepoint."""
-
+    # parse a hexadecimal Unicode codepoint
     try:
         codepoint = int(codepoint, 16)
-        if not 0x0 <= codepoint <= 0x10ffff:
+        if not 0 <= codepoint <= 0x10ffff:
             raise ValueError
     except ValueError:
-        sys.exit("Invalid Unicode codepoint.")
+        sys.exit("Invalid codepoint.")
     return codepoint
 
 def parse_codepoint_range(range_):
-    """Parse a hexadecimal Unicode codepoint or a range. Return a range()."""
-
+    # parse a hexadecimal Unicode codepoint or a range, return a range()
     items = range_.split("-")
-    first = parse_codepoint(items[0])
-    if len(items) == 1:
-        last = first
-    elif len(items) == 2:
-        last = parse_codepoint(items[1])
-        if first > last:
-            sys.exit("First codepoint greater than last one in codepoint range.")
-    else:
-        sys.exit("More than one hyphen in codepoint range.")
+    if len(items) != 2:
+        sys.exit("Invalid codepoint range.")
+    (first, last) = (parse_codepoint(i) for i in items)
+    if first > last:
+        sys.exit("Invalid codepoint range.")
     return range(first, last + 1)
 
-def get_alphabet(settings):
-    """Get the set of characters to use in passwords."""
-
+def get_alphabet(args):
+    # get the set of characters to use in passwords
     alphabet = set()
-    if "u" in settings.sets:
-        alphabet.update(settings.uppercase)
-    if "l" in settings.sets:
-        alphabet.update(settings.lowercase)
-    if "d" in settings.sets:
-        alphabet.update(settings.digits)
-    if "p" in settings.sets:
-        alphabet.update(settings.punctuation)
-    if "n" in settings.sets:
-        for range_ in settings.unicode.split(","):
+    if "u" in args.sets:
+        alphabet.update(args.uppercase)
+    if "l" in args.sets:
+        alphabet.update(args.lowercase)
+    if "d" in args.sets:
+        alphabet.update(args.digits)
+    if "p" in args.sets:
+        alphabet.update(args.punctuation)
+    if "n" in args.sets:
+        for range_ in args.unicode.split(","):
             alphabet.update(chr(codepoint) for codepoint in parse_codepoint_range(range_))
     if not alphabet:
         sys.exit("No characters in selected sets.")
     return alphabet
 
-def main():
-    """The main function."""
-
-    settings = parse_arguments()
-    alphabet = get_alphabet(settings)
-    print("".join(secrets.choice(list(alphabet)) for i in range(settings.length)))
-
-if __name__ == "__main__":
-    main()
+args = parse_arguments()
+alphabet = list(get_alphabet(args))
+print("".join(secrets.choice(alphabet) for i in range(args.length)))
